8c27fa76888edaf855f6f2d20c1d17bbf7358503
JoaoPauloCSA <jp10alencar@gmail.com>
2025-01-10 07:14:25 -0300

    Add files via upload

---
+++ SemaForms/Paralela.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices; // tem que colocar
+
+
+namespace SemaForms
+    {
+        class Paralela
+        {
+            // Escreve um byte no endereço
+            [DllImport("Inpout32.dll", EntryPoint = "Out32")]
+            public static extern void Escrever(int endereco, byte valor);
+        }
+    }
+
+
